622212ce36aa59ef139163accb134a22de4a03bd
Bowden <robbie.bowden@yahoo.com>
2018-03-08 13:10:58 -0500

    Basic game function

---
+++ NodeQuizGame/Assets/Scripts/AnswerButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour {
+    public Text answerText;
+    private AnswerData answerData;
+    private GameController gameController;
+
+	// Use this for initialization
+	void Start () {
+        answerText = GetComponentInChildren<Text>();
+        gameController = FindObjectOfType<GameController>();
+	}
+	
+    public void SetUp(AnswerData data)
+    {
+        answerData = data;
+        answerText.text = data.answerText;
+    }
+
+    public void Clicked()
+    {
+        gameController.AnswerClicked(answerData.isCorrect);
+    }
+}
---
+++ NodeQuizGame/Assets/Scripts/GameController.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    private int playerScore;
+    private float timer;
+
+    public Text questionText;
+    public Text scoreText;
+    public Text timeText;
+
+    public GameObject questionDisplay;
+    public GameObject endGameDisplay;
+
+    private DataController dataController;
+    private RoundData roundData;
+    private QuestionData[] questionData;
+    private bool isRoundActive;
+    private int questionIndex = 0;
+
+    public Transform answersPanel;
+
+    private BasicObjectPool answerButtonPool;
+    private List<GameObject> answerButtonObjects = new List<GameObject>();
+
+    // Use this for initialization
+    void Start() {
+
+        dataController = FindObjectOfType<DataController>();
+        answerButtonPool = FindObjectOfType<BasicObjectPool>();
+        roundData = dataController.GetCurrentRoundData();
+        questionData = roundData.questions;
+        ShowQuestions();
+        playerScore = 0;
+        timer = roundData.timeLimitInSeconds;
+        isRoundActive = true;
+    }
+
+    void Update()
+    {
+        if(isRoundActive)
+            UpdateTime();
+    }
+
+    private void ShowQuestions()
+    {
+        RemoveAnswerButtons();
+        QuestionData question = questionData[questionIndex];
+        questionText.text = question.questionText;
+        for (int i = 0; i < question.answers.Length; i++)
+        {
+            GameObject answer = answerButtonPool.GetObject();
+
+            answer.transform.SetParent(answersPanel, false);
+            answer.SetActive(true);
+            answerButtonObjects.Add(answer);
+
+            AnswerButton answerButton = answer.GetComponent<AnswerButton>();
+            answerButton.SetUp(question.answers[i]);
+        }
+
+    }
+
+    private void RemoveAnswerButtons()
+    {
+        while(answerButtonObjects.Count > 0)
+        {
+            answerButtonPool.ReturnObject(answerButtonObjects[0]);
+            answerButtonObjects.RemoveAt(0);
+        }
+    }
+
+    private void UpdateTime()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+            EndRound();
+
+        string minutes = Mathf.Floor(timer / 60).ToString("00");
+        string seconds = Mathf.Floor(timer % 60).ToString("00");
+
+        timeText.text = "Timer: " + minutes + ":" + seconds;
+    }
+
+    public void EndRound()
+    {
+        isRoundActive = false;
+
+        questionDisplay.SetActive(false);
+        endGameDisplay.SetActive(true);
+
+    }
+
+    public void StartOver()
+    {
+        SceneManager.LoadScene("MenuScreen");
+    }
+
+    public void AnswerClicked(bool isCorrect)
+    {
+        // If correct answer
+        if (isCorrect)
+        {
+            // Add to player score
+            playerScore += roundData.pointsAddedForCorrectAnswer;
+            scoreText.text = "Score: " + playerScore.ToString();
+        }
+
+        // If there are more questions
+        if(questionData.Length > questionIndex + 1)
+        {
+            questionIndex++;
+            ShowQuestions();
+        }
+        else // Last question
+        {
+            // End Game
+            EndRound();
+        }
+    }
+	
+}
